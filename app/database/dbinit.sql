a9877e96f2185d83282f0620fd436fdc8e41870b
LeoCurdi <ldcurdi@icloud.com>
2025-03-04 17:50:30 -0800

    Fix dbinit

--- app/database/dbinit.sql
+++ app/database/dbinit.sql
@@ -1,19 +1,11 @@
-CREATE TABLE Admin (
+CREATE TABLE if not exists Admin (
     ID VARCHAR(255) PRIMARY KEY,
     firstName VARCHAR(255) NOT NULL,
     lastName VARCHAR(255) NOT NULL
 );
 
 
-CREATE TABLE Student (
-    ID VARCHAR(255) PRIMARY KEY,
-    roomID VARCHAR(255) REFERENCES Room(ID),
-    firstName VARCHAR(255) NOT NULL,
-    lastName VARCHAR(255) NOT NULL,
-);
-
-
-CREATE TABLE Room (
+CREATE TABLE if not exists Room (
     ID VARCHAR(255) PRIMARY KEY,
     Name VARCHAR(255) NOT NULL,
     Description TEXT,
@@ -21,7 +13,15 @@ CREATE TABLE Room (
 );
 
 
-CREATE TABLE RoomRequest (
+CREATE TABLE if not exists Student (
+    ID VARCHAR(255) PRIMARY KEY,
+    roomID VARCHAR(255) REFERENCES Room(ID),
+    firstName VARCHAR(255) NOT NULL,
+    lastName VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE if not exists RoomRequest (
     requestID INT PRIMARY KEY,
     studentId VARCHAR(255) REFERENCES Student(ID),
     roomId VARCHAR(255) REFERENCES Room(ID),
@@ -30,7 +30,7 @@ CREATE TABLE RoomRequest (
 );
 
 
-CREATE TABLE MaintenanceRequest (
+CREATE TABLE if not exists MaintenanceRequest (
     requestID INT PRIMARY KEY,
     studentId VARCHAR(255) REFERENCES Student(ID),
     roomId VARCHAR(255) REFERENCES Room(ID),
@@ -40,7 +40,7 @@ CREATE TABLE MaintenanceRequest (
 );
 
 
-CREATE TABLE RoommateRequest (
+CREATE TABLE if not exists RoommateRequest (
     requestID INT PRIMARY KEY,
     requesterId VARCHAR(255) REFERENCES Student(ID),
     requesteeId VARCHAR(255) REFERENCES Student(ID),
@@ -49,7 +49,7 @@ CREATE TABLE RoommateRequest (
 );
 
 
-CREATE TABLE AdminRoomRequestManagement (
+CREATE TABLE if not exists AdminRoomRequestManagement (
     managementID VARCHAR(255) PRIMARY KEY,
     adminID VARCHAR(255) REFERENCES Admin(ID),
     requestID INT REFERENCES RoomRequest(requestID)
