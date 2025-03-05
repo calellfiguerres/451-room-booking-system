aa75d9c103e256e3b03685d07e1e4ce6c2cccd4b
LeoCurdi <ldcurdi@icloud.com>
2025-03-04 21:01:39 -0800

    Add api route for db

--- app/server/src/Controller/adminProcedures.ts
+++ app/server/src/Controller/adminProcedures.ts
@@ -5,8 +5,13 @@ export const adminProcedures = router({
     hello: publicProcedure.query(async (opts) => {
         return "hello from admin procedures!";
     }), 
-    getUsers: publicProcedure.query(async (opts) => {
+    getAdmins: publicProcedure.query(async (opts) => {
         const result = db.connection.any('SELECT * FROM admin');
+        console.log(result);
+        return result;
+    }), 
+    getStudents: publicProcedure.query(async (opts) => {
+        const result = db.connection.any('SELECT * FROM student');
         console.log(result);
         return result;
     })
