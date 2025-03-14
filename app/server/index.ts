a2b9bf7d33a94db68834e9bfbb853e6553099f16
Calell Figuerres <142842875+calellfiguerres@users.noreply.github.com>
2025-03-13 19:55:08 -0700

    Added cookie parser to app initialization

--- app/server/index.ts
+++ app/server/index.ts
@@ -1,10 +1,12 @@
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
+import cookieParser from "cookie-parser";
 import express from "express";
 import { createContext } from "./src/Controller/trpc";
 import { procedures } from "./src/Controller/router";
 
 const app = express();
 
+app.use(cookieParser("totally secure secret"));
 app.use("/api", createExpressMiddleware({
     router: procedures,
     createContext
