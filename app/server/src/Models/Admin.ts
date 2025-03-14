864df28061e46b9b383f3249cadd338094665f52
Calell Figuerres <142842875+calellfiguerres@users.noreply.github.com>
2025-03-13 20:04:38 -0700

    Added `getById` and `getByUsername` static methods to `Admin` and `Student`

--- app/server/src/Models/Admin.ts
+++ app/server/src/Models/Admin.ts
@@ -14,11 +14,27 @@ export class Admin {
     }
 
     public static async getAll() {
-        const response = await db.connection.any("SELECT * FROM ADMIN");
+        const response = await db.connection.any("SELECT * FROM admin");
         const result = response.map((r) => new Admin(
             r.id, r.username, r.password, r.firstname, r.lastname
         ));
         console.log(result);
+        return result;
+    }
+
+    public static async getById(id: string) {
+        const response = await db.connection.one("SELECT * FROM admin WHERE ID = $1", [id]);
+        const result = new Admin(
+            response.id, response.username, response.password, response.firstname, response.lastname
+        );
+        return result;
+    }
+
+    public static async getByUsername(username: string) {
+        const response = await db.connection.one("SELECT * FROM admin WHERE username = $1", [username]);
+        const result = new Admin(
+            response.id, response.usernae, response.password, response.firstname, response.lastname
+        );
         return result;
     }
 }
--- app/server/src/Models/Student.ts
+++ app/server/src/Models/Student.ts
@@ -10,12 +10,35 @@ export class Student {
         public roomid: string
     ) {}
 
+    public checkPassword(input: string): boolean {
+        return input === this.password;
+    }
+
     public static async getAll() {
         const response = await db.connection.any("SELECT * FROM student");
         const result = response.map((r) => new Student(
             r.id, r.username, r.password, r.firstname, r.lastname, r.roomid
         ));
         console.log(result);
+        return result;
+    }
+
+    public static async getById(id: string) {
+        const response = await db.connection.one("SELECT * FROM student WHERE id = $1", [id]);
+        const result = new Student(
+            response.id, response.username, response.password, response.firstname, response.lastname, response.roomid
+        );
+        return result;
+    }
+
+    public static async getByUsername(username: string) {
+        console.log("before - student");
+        const response = await db.connection.one("SELECT * FROM student WHERE username = $1", [username]);
+        console.log(`response: ${response}`);
+        console.log("after");
+        const result = new Student(
+            response.id, response.username, response.password, response.firstname, response.lastname, response.roomid
+        );
         return result;
     }
 }
