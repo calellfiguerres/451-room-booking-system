bf2531cb249fb5cfad7a42183eb3cc97d9ad4162
Calell Figuerres <142842875+calellfiguerres@users.noreply.github.com>
2025-03-13 22:59:43 -0700

    Switched `admin.getAdmins` and `admin.getStudents` to use `adminOnlyProcedure` middleware

--- app/server/src/Controller/adminProcedures.ts
+++ app/server/src/Controller/adminProcedures.ts
@@ -1,5 +1,5 @@
 import z from "zod";
-import { publicProcedure, router } from "./trpc";
+import { adminOnlyProcedure, authenticatedProcedure, publicProcedure, router } from "./trpc";
 import { Admin } from "../Models/Admin";
 import { Student } from "../Models/Student";
 import { Session } from "../Models/Session";
@@ -10,14 +10,14 @@ export const adminProcedures = router({
     hello: publicProcedure.query(async (opts) => {
         return "hello from admin procedures!";
     }),
-    getAdmins: publicProcedure.query(async (opts) => {
+    getAdmins: adminOnlyProcedure.query(async (opts) => {
         const result = Admin.getAll();
-        console.log(result);
+        // console.log(result);
         return result;
     }), 
-    getStudents: publicProcedure.query(async (opts) => {
+    getStudents: adminOnlyProcedure.query(async (opts) => {
         const result = Student.getAll();
-        console.log(result);
+        // console.log(result);
         return result;
     })
 })
