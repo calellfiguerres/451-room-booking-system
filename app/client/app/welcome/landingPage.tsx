e01892dd53abe550084927443b5bcc30f2fa85ba
LeoCurdi <ldcurdi@icloud.com>
2025-04-01 16:37:52 -0700

    Add notification button

--- app/client/app/welcome/landingPage.tsx
+++ app/client/app/welcome/landingPage.tsx
@@ -1,5 +1,6 @@
 
 import Box from "~/Components/Box";
+import NotificationBox from "~/Components/NotificationBox";
 
 export function LandingPage() {
 
@@ -7,7 +8,10 @@ export function LandingPage() {
   return (
     <div className='w-full pb-10 bg-black/10 height-minus-nav'>
         <div className='max-w-[1240px] py-8 px-2 mx-auto space-y-4'>
-            <div className='max-w-[1240px] py-8 px-16 mx-auto '>
+                <div className="float-right absolute top-10 right-10 px-2 rounded-lg text-center">
+                    <NotificationBox link="/usersList" text="notifications" image="/public/bell.png"/>
+                </div>
+            <div className='max-w-[1240px] py-8 px-16 mx-auto'>
                 <div className='grid sm:grid-cols-1 lg:grid-cols-1 gap-8 my-4'>
                     <Box link="/usersList" text="View users"/>
                     <Box link="/reservations" text="Room Reservations"/>
