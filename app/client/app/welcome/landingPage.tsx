49dcbda81ccfdd75e507ce1580fddbbd58f42ae1
Calell Figuerres <142842875+calellfiguerres@users.noreply.github.com>
2025-04-01 16:23:32 -0700

    Added login, logout, signup, and maintenance list buttons to landing page

--- app/client/app/welcome/landingPage.tsx
+++ app/client/app/welcome/landingPage.tsx
@@ -9,8 +9,12 @@ export function LandingPage() {
         <div className='max-w-[1240px] py-8 px-2 mx-auto space-y-4'>
             <div className='max-w-[1240px] py-8 px-16 mx-auto '>
                 <div className='grid sm:grid-cols-1 lg:grid-cols-1 gap-8 my-4'>
+                    <Box link="/login" text="Login" />
+                    <Box link="/logout" text="Logout" />
+                    <Box link="/signup" text="Signup" />
                     <Box link="/usersList" text="View users"/>
                     <Box link="/reservations" text="Room Reservations"/>
+                    <Box link="/maintenanceList" text="Maintenance List" />
                 </div>
             </div>
         </div>
