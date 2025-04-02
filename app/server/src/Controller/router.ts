8a71691e211f184188d6cfa0ebbd2a56a749369b
Grace Anderson <grace.anderson@lumen.com>
2025-04-01 17:36:49 -0700

    registered room procedures in main router
    - imported roomProcedures model
    - added room procedures to main procedures router

--- app/server/src/Controller/router.ts
+++ app/server/src/Controller/router.ts
@@ -2,13 +2,15 @@ import { adminProcedures } from "./adminProcedures";
 import { authProcedures } from "./authProcedures";
 import { maintenanceProcedures } from "./maintenanceProcedures";
 import { bookingProcedures } from "./bookingProcedures";
+import { roomProcedures } from "./roomProcedures";
 import { router } from "./trpc";
 
 export const procedures = router({
     auth: authProcedures,
     admin: adminProcedures,
     maintenance: maintenanceProcedures,
-    reservation: bookingProcedures
+    reservation: bookingProcedures,
+    rooms: roomProcedures
 });
 
 export type Procedures = typeof procedures;
