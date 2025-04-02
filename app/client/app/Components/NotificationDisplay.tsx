07ae969f779ae7c29d4d912b9033029e98692c06
LeoCurdi <ldcurdi@icloud.com>
2025-04-01 17:37:12 -0700

    Improve notification display

--- app/client/app/Components/NotificationDisplay.tsx
+++ app/client/app/Components/NotificationDisplay.tsx
@@ -10,18 +10,24 @@ const NotificationDisplay: React.FC<{
 
 
         <div className="p-4 block space-x-4 flex justify-between items-center preview-button">
-            <p className="text-xl font-bold">
-                {id}
-            </p>
-            <p className="text-lg">
-                {studentID}
-            </p>
-            <p className="text-lg">
-                {studentName}
-            </p>
-            <p className="text-lg">
-                {text}
-            </p>
+            <div className="grid grid-cols-2 gap-4">
+                <div className="grid grid-cols-1 gap-4">
+                    <p className="text-xl font-bold">
+                        {id}
+                    </p>
+                    <p className="text-lg">
+                        {studentID}
+                    </p>
+                    <p className="text-lg">
+                        {studentName}
+                    </p>
+                </div>
+                <div>
+                    <p className="text-lg">
+                        {text}
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
 )
