1bc3aee0b5b67b4827f71a14dc58caa88d165b32
aaronmhowe <aaron.howe99@gmail.com>
2025-04-03 13:59:20 -0700

    created skeleton structure for new entities

---
+++ app/client/app/Components/ReservationApplication.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import { api } from "~/.client/Providers/trpc";
+import { useNavigate } from "react-router";
+import type { Room } from "../../../server/src/Models/Room";
+
+const ReservationApplication: React.FC = () => {
+
+    useEffect(() => {
+
+    }, []);
+
+    const appSubmission = async (e: React.FormEvent) => {
+
+    };
+
+    return (
+        <div className="">
+
+        </div>
+    );
+};
+
+export default ReservationApplication;
---
+++ app/client/app/routes/viewApplicationForm.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReservationApplication from "~/Components/ReservationApplication";
+
+export default function Application() {
+    return (
+        <div className="">
+
+        </div>
+    );
+};
