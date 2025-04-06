8508606b5322cd87a564d2b5e7369636ac865198
jevans <joshua.d.evans@wsu.edu>
2025-04-05 21:38:41 -0700

    login/signin Button

--- app/client/app/Components/LoginSignupBox.tsx
+++ app/client/app/Components/LoginSignupBox.tsx
@@ -3,9 +3,10 @@ import { Link } from "react-router-dom";
 
 interface LoginSignupBoxProps {
   loginLink: string;
+  signupLink: string;
 }
 
-const LoginSignupBox: React.FC<LoginSignupBoxProps> = ({ loginLink }) => {
+const LoginSignupBox: React.FC<LoginSignupBoxProps> = ({ loginLink, signupLink }) => {
   return (
     <div className="
       p-3 bg-gray-100 rounded-lg
@@ -22,8 +23,20 @@ const LoginSignupBox: React.FC<LoginSignupBoxProps> = ({ loginLink }) => {
           focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500
         "
       >
-        Login/Signup
-      </Link>      
+        Login
+      </Link>
+
+      <Link
+        to={signupLink}
+        className="
+          px-4 py-1.5 bg-green-500 text-white text-sm font-medium rounded-md
+          hover:bg-green-600
+          transition duration-150 ease-in-out
+          focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500
+        "
+      >
+        Sign Up
+      </Link>
     </div>
   );
 };
--- app/client/app/welcome/landingPage.tsx
+++ app/client/app/welcome/landingPage.tsx
@@ -1,29 +1,49 @@
+import { useEffect, useState } from "react";
 import Box from "~/Components/Box";
 import NotificationBox from "~/Components/NotificationBox";
 import LoginSignupBox from '~/Components/LoginSignupBox';
+import LogoutBox from '~/Components/LogoutBox';
+import { api } from "~/.client/Providers/trpc";
 
 export function LandingPage() {
+ 
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+    useEffect(() => {
+        api.auth.checkAuth.query(undefined,{}).then((res) =>{
+            setIsLoggedIn(res);
+        });
+    },[])
+   
   const loginPath = "/login";
+  const logoutPath = "/logout"; 
+  const notificationsPath = "/notifications";
+  const usersListPath = "/usersList";
+  const reservationsPath = "/reservations";
   const signupPath = "/signup";
 
   return (
     <div className='w-full pb-10 bg-black/10 height-minus-nav relative'>
 
+      {}
       <div className="absolute top-5 right-5 flex items-center space-x-3 z-10">
-
-        <LoginSignupBox loginLink={loginPath} />
-
-        <NotificationBox link="/notifications" text="notifications" image="/public/bell.png" />
-
+       
+        {}
+        {isLoggedIn ? (
+          <>
+            <LogoutBox LogoutLink={logoutPath} />
+            <NotificationBox link={notificationsPath} text="notifications" image="/public/bell.png" />
+          </>
+        ) : (
+          <LoginSignupBox loginLink={loginPath} signupLink={signupPath} />
+        )}        
       </div>
 
       <div className='max-w-[1240px] py-8 px-2 mx-auto space-y-4'>
-
         <div className='max-w-[1240px] py-8 px-16 mx-auto pt-16'>
           <div className='grid sm:grid-cols-1 lg:grid-cols-1 gap-8 my-4'>
-            <Box link="/usersList" text="View users" />
-            <Box link="/reservations" text="Room Reservations" />
+            <Box link={usersListPath} text="View users" />
+            <Box link={reservationsPath} text="Room Reservations" />
           </div>
         </div>
       </div>
