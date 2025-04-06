f4b5dadb684ba189ef1a515eb68897a544d05a8a
Calell Figuerres <142842875+calellfiguerres@users.noreply.github.com>
2025-04-06 00:39:41 -0700

    Added roommate request page (form to submit requests and ability to view, but not accept/decline, requests)

--- app/client/app/welcome/landingPage.tsx
+++ app/client/app/welcome/landingPage.tsx
@@ -14,6 +14,7 @@ export function LandingPage() {
                     <Box link="/signup" text="Signup" />
                     <Box link="/usersList" text="View users"/>
                     <Box link="/reservations" text="Room Reservations"/>
+                    <Box link="/roommateRequests" text="Roommate Requests" />
                     <Box link="/maintenanceList" text="Maintenance List" />
                 </div>
             </div>
