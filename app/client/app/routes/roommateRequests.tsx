e453ec8379783738b42e27a11fec400d39c0a9b9
Calell Figuerres <142842875+calellfiguerres@users.noreply.github.com>
2025-04-06 00:57:05 -0700

    Added home box to roommate requests page

--- app/client/app/routes/roommateRequests.tsx
+++ app/client/app/routes/roommateRequests.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { api } from "~/.client/Providers/trpc";
+import HomeBox from "~/Components/HomeBox";
 
 interface Roommate {
     firstname: string;
@@ -47,43 +48,48 @@ export default function roommateRequests() {
     }
 
     return (
-        <div className='w-full pb-10 bg-black/10 height-minus-nav'>
-            <div className='max-w-[1240px] py-8 px-2 mx-auto space-y-4'>
-                <div className="rounded-md bg-white py-8 my-4">
-                    <p className='text-2xl text-center tracking-widest'>
-                        Roommate Requests
-                    </p>
-                </div>
+        <>
+            <div className="absolute top-5 right-5 flex items-center space-x-3 z-10">             
+                <HomeBox homeLink="/" />
             </div>
-            <ul className="px-5 list-disc">
-                {
-                    roommateRequests.map((r) => (
-                        <li key={r.requestid}>
-                            <p>To: {r.requestee?.firstname} {r.requestee?.lastname}</p>
-                            <p>From: {r.requester?.firstname} {r.requester?.lastname}</p>
-                            <p>Message: {r.message}</p>
-                            <p>Sent: {r.sendDate}</p>
-                        </li>
-                    ))
-                }
-            </ul>
-            <hr />
-            <form onSubmit={(e) => { e.preventDefault(); e.stopPropagation(); submitRequest(); }}>
-                <label htmlFor="roommateSelect" className="text-xl px-5">Select Roommate</label>
-                <select name="roommateId" id="roommateSelect" value={requestedRoommateId} onChange={(e) => setRequestedRoommateId(e.target.value)}>
-                    <option value="none">No Roommate Selected</option>
+            <div className='w-full pb-10 bg-black/10 height-minus-nav'>
+                <div className='max-w-[1240px] py-8 px-2 mx-auto space-y-4'>
+                    <div className="rounded-md bg-white py-8 my-4">
+                        <p className='text-2xl text-center tracking-widest'>
+                            Roommate Requests
+                        </p>
+                    </div>
+                </div>
+                <ul className="px-5 list-disc">
                     {
-                        potentialRoommates.map((r) => (
-                            <option key={r.id} value={r.id}>{r.firstname} {r.lastname}</option>
+                        roommateRequests.map((r) => (
+                            <li key={r.requestid}>
+                                <p>To: {r.requestee?.firstname} {r.requestee?.lastname}</p>
+                                <p>From: {r.requester?.firstname} {r.requester?.lastname}</p>
+                                <p>Message: {r.message}</p>
+                                <p>Sent: {r.sendDate}</p>
+                            </li>
                         ))
                     }
-                </select>
-                <br />
-                <label htmlFor="message" className="text-xl px-5">Message</label>
-                <textarea name="message" className="w-lg bg-white rounded-md" value={requestMessage} onChange={(e) => setRequestMessage(e.target.value)}></textarea>
-                <br />
-                <button type="submit" className="rounded-md bg-white mx-2 px-2 hover:border-2">Submit Roommate Request</button>
-            </form>
-        </div>
+                </ul>
+                <hr />
+                <form onSubmit={(e) => { e.preventDefault(); e.stopPropagation(); submitRequest(); }}>
+                    <label htmlFor="roommateSelect" className="text-xl px-5">Select Roommate</label>
+                    <select name="roommateId" id="roommateSelect" value={requestedRoommateId} onChange={(e) => setRequestedRoommateId(e.target.value)}>
+                        <option value="none">No Roommate Selected</option>
+                        {
+                            potentialRoommates.map((r) => (
+                                <option key={r.id} value={r.id}>{r.firstname} {r.lastname}</option>
+                            ))
+                        }
+                    </select>
+                    <br />
+                    <label htmlFor="message" className="text-xl px-5">Message</label>
+                    <textarea name="message" className="w-lg bg-white rounded-md" value={requestMessage} onChange={(e) => setRequestMessage(e.target.value)}></textarea>
+                    <br />
+                    <button type="submit" className="rounded-md bg-white mx-2 px-2 hover:border-2">Submit Roommate Request</button>
+                </form>
+            </div>
+        </>
     );
 }
