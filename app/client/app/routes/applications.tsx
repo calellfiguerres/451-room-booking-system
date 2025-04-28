7a6bb66ac6f6071414dd0b3f4c6bd9ecf3e596ac
aaronmhowe <aaron.howe99@gmail.com>
2025-04-27 22:05:05 -0700

    creating new route for the application form page

---
+++ app/client/app/routes/applications.tsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+import { api } from "~/.client/Providers/trpc";
+import HomeBox from "~/Components/HomeBox";
+import { useNavigate } from "react-router";
+
+/**
+ * Primary function for the application form page.
+ */
+export default function ApplicationForm() {
+
+}
