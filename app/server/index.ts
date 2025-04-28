ee929ef526eb97b516fcab7d03ba9921404815be
Grace Anderson <grace.anderson@lumen.com>
2025-04-27 17:12:52 -0700

    added SSE endpoint to server
    - added /api/events endpoint for real-time connections

--- app/server/index.ts
+++ app/server/index.ts
@@ -3,6 +3,7 @@ import cookieParser from "cookie-parser";
 import express from "express";
 import { createContext } from "./src/Controller/trpc";
 import { procedures } from "./src/Controller/router";
+import { handleSSE } from './src/Controller/eventsProcedures';
 
 const app = express();
 
@@ -15,3 +16,5 @@ app.use("/api", createExpressMiddleware({
 app.listen(8001, () => {
     console.log("Listening on http://localhost:8001");
 });
+
+app.get('/api/events', handleSSE);
